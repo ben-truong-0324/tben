98fe5058b59c74a2f63b20df34b64e816b640e3d
unknown <ben.truong.0324@gmail.com>
2020-09-25 20:18:06 -0700

    fixed settings for static assets find

--- personalsite/settings/production.py
+++ personalsite/settings/production.py
@@ -5,6 +5,14 @@ DATABASES = {
 }
 SECURE_SSL_REDIRECT = True
 
-STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'personalize/static'),)
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
